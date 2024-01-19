e56dd9e8021ef48b151424c8049107c70a319c6b
itsMeVela <velaputri681@gmail.com>
2024-01-19 16:08:10 +0700

    color tombol yuk kenal lebih dekat

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textKu: '#C26582',
+        dark: '#1e293b'
+      }
+    },
   },
   plugins: [],
 }
