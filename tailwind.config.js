442b20b72313b4125609e6e6101d5eeb6b7b0881
itsMeVela <velaputri681@gmail.com>
2024-01-19 17:39:35 +0700

    section header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
         textKu: '#C26582',
         dark: '#1e293b'
+      },
+      screens: {
+        '2xl': '1320px'
       }
     },
   },
